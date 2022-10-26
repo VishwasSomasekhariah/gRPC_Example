21f7bfd0c6d08e1f083d8dcc9beea726eb9e8895
Vishwas Prasanna <vkumar@intelops.dev>
2022-10-26 17:04:59 -0500

    Updated the column name that the client.py wants to read from the server

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import grpc
 import example_pb2
 import example_pb2_grpc
+from pprint import pprint
 
 def run_client():
 	with grpc.insecure_channel('localhost:20001') as channel:
-		stub = grpc.example_pb2_grpc.ComputatorStub(channel)
-		response = stub.getMeInformation(example_pb2.getMeDataRequest(col_name = "something_goes_here"))
+		stub = example_pb2_grpc.ComputatorStub(channel)
+		response = stub.getMeInformation(example_pb2.getMeDataRequest(col_name = "Sepal_Length"))
 		print("Client recieved the response successfully:")
-		print(response)
+		print(bytes(str(response),"utf-8").decode("unicode_escape"))
 
 if __name__ == '__main__':
 	run_client()
